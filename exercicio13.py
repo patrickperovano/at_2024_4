f15ba353a899e42cb5292bfcb1545ac44c7302a5
Mateus Oliveira <mtsodf@gmail.com>
2024-11-22 21:04:33 +0100

    Exercicio 13

---
+++ exercicio13.py
@@ -0,0 +1,25 @@
+# Exercicio
+"""
+Você recebeu um arquivo de texto chamado 'entrada.txt' que contém várias linhas. Seu objetivo é criar um programa que 
+remova todas as linhas duplicadas desse arquivo e escreva cada linha única em um novo arquivo chamado 'saida.txt'. 
+Siga as instruções abaixo:
+
+1. Leia o conteúdo do arquivo 'entrada.txt'.
+2. Remova as linhas duplicadas.
+3. Escreva as linhas únicas no arquivo 'saida.txt'.
+
+Certifique-se de que o arquivo 'saida.txt' seja criado e que cada linha única apareça apenas uma vez.
+"""
+
+
+def remover_linhas_duplicadas(entrada, saida):
+    pass  # Implemente sua solução aqui.
+
+
+def main():
+    # Chame a função com os nomes dos arquivos
+    remover_linhas_duplicadas("entrada13.txt", "saida_duplicadas.txt")
+
+
+if __name__ == "__main__":
+    main()
