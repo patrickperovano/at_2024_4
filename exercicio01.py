130348f658a239c1e849d7b2e670d2923989b4a2
Mateus Oliveira <mtsodf@gmail.com>
2024-11-22 19:56:10 +0100

    Questao 01

---
+++ exercicio01.py
@@ -0,0 +1,26 @@
+# Exercício
+"""
+O código abaixo apresenta um problema relacionado ao tratamento de exceções em Python. Analise o seguinte código e responda à pergunta:
+
+"""
+
+
+def main():
+    # Uma função genérica qualquer
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("O código não funcionou corretamente")
+
+
+""""
+Por que esse código pode ser considerado uma má prática quando se trata de tratamento de exceções? Que problemas podem surgir ao utilizar um bloco `except` sem especificar o tipo de exceção?
+"""
+
+# Resposta:
+"""
+"""
