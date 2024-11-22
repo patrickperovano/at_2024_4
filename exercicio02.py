da355ebd8a10366ddfa1e70cb4885880135c9ecb
Mateus Oliveira <mtsodf@gmail.com>
2024-11-22 19:58:05 +0100

    Exercicio 02

---
+++ exercicio02.py
@@ -0,0 +1,28 @@
+# Exercicio
+"""
+Criar uma função chamada dividir que recebe dois números como parâmetros e retorna o resultado da divisão do primeiro número pelo segundo. 
+Se o segundo número for zero, a função deve retornar a mensagem "Não é possível dividir por zero". 
+Utilize um bloco try-except para tratar a exceção de divisão por zero.
+"""
+
+
+def dividir(num1, num2):
+    pass  # Implemente a lógica da função aqui
+
+
+def main():
+    num1 = 10
+    num2 = 0
+
+    resultado = dividir(num1, num2)
+    print("Resultado:", resultado)
+
+    num1 = 10
+    num2 = 2
+
+    resultado = dividir(num1, num2)
+    print("Resultado:", resultado)
+
+
+if __name__ == "__main__":
+    main()
