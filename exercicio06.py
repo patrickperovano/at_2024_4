eb7eef5afd94415ffe7add0d85557dd137b2ddaf
Mateus Oliveira <mtsodf@gmail.com>
2024-11-22 20:12:19 +0100

    Exercicio 06

---
+++ exercicio06.py
@@ -0,0 +1,24 @@
+# Exercicio
+"""
+Escreva um programa em Python que leia o arquivo 'alice.txt' e conte quantas vezes a palavra "Alice" aparece no texto.
+Para isso, utilize as funções de abertura e leitura de arquivos. Em seguida, imprima o resultado na tela.
+Certifique-se de que a contagem não depende de maiúsculas ou minúsculas (ou seja, considere 'Alice', 'alice' e 'ALICE' como a mesma palavra).
+
+Dicas:
+- Utilize o método .lower() para facilitar a contagem de forma case insensitive.
+- Considere que a palavra "Alice" pode aparecer em diferentes contextos e pontuações.
+"""
+
+def contar_a_palavra_alice():
+    pass
+
+def main():
+    quantidade = contar_a_palavra_alice()
+    if quantidade
+        print(quantidade)
+    else:
+        print(f"A palavra 'Alice' aparece {quantidade} vezes no texto.")
+
+if __name__ == "__main__":
+    main()
+
