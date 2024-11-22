b4bcd51c17bfd56460e6efd030267ddb2d709742
Mateus Oliveira <mtsodf@gmail.com>
2024-11-22 20:05:46 +0100

    Exercicio 04

---
+++ exercicio04.py
@@ -0,0 +1,30 @@
+# Exercicio
+"""
+Você está trabalhando em um sistema que gerencia listas de compras de usuários. Abaixo estão duas listas: 
+uma contém os itens que um usuário já possui e a outra contém os itens que ele deseja comprar. 
+
+Seu objetivo é encontrar quais itens estão na lista de desejos, mas não na lista de posses (ou seja, os itens que ele ainda não possui). 
+
+Complete a função itens_faltantes que deve retornar a diferença entre os dois conjuntos (sets):
+1. items_possuidos: uma lista com os itens que o usuário já possui.
+2. items_desejados: uma lista com os itens que o usuário deseja comprar.
+
+Dica: Utilize a operação de diferença entre sets do Python.
+"""
+
+
+def itens_faltantes(items_possuidos, items_desejados):
+    return None
+
+
+# Versão de teste
+def main():
+    possuidos = {"maçã", "banana", "laranja"}
+    desejados = {"banana", "uva", "melancia", "maçã"}
+
+    faltando = itens_faltantes(possuidos, desejados)
+    print(f"Itens faltando na lista de compras: {faltando}")
+
+
+if __name__ == "__main__":
+    main()
